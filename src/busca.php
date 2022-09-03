78c6d2ea4ac3f8a5a0e51e4acd763da11904f8a3
DafneM <moretti.dafne2@gmail.com>
2022-09-03 12:21:37 -0300

    Atualizando aplicacao

--- src/busca.php
+++ src/busca.php
@@ -5,18 +5,77 @@ ini_set("display_erros", 1);
 if($_SERVER['REQUEST_METHOD'] == 'GET'){
     $link = mysqli_connect('127.0.0.1', 'root', '171000', 'ccs');
     $nome = $_GET['nome_produto'];
-    $query = "SELECT productID FROM products WHERE Description LIKE '%$nome%'";
+    $query = "SELECT Description FROM products WHERE Description LIKE '%$nome%'";
     $result = mysqli_query($link, $query);
-    echo"<pre>";
-    print_r($result);exit;
+    /* $results = [];
+    while ($obj=mysqli_fetch_object($result)){
+        $results[] = $obj;
+    } */
+    //echo"<pre>";
+    //print_r($result);exit;
 }
 
 ?>
 
 <html>
     <head>
-        <h2>Resultado</h2>
+        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+        <h2 class="title-resultado">Resultado</h2>
     </head>
     <body>
+    <table>
+        <tr>
+            <th>Nome do produto</th>
+        </tr>
+        <tr>
+
+        <?php
+            foreach($result as $product){
+                $descricao = $product['Description'];
+        ?>
+            <td class="table-item"><?=$descricao?></td>
+        </tr>
+        <?php } ?>
+        </table>
     </body>
 </html>
+
+<style>
+    
+    body {
+        min-height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        font-family: 'Poppins';
+        background-color: #F8F8FF;
+    }
+
+    .title-resultado{
+        color: #D2691E;
+        font-size: 2rem;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 50%;
+        background-color: white;
+    }
+
+    table, th, td {
+        border: 0.1px solid #A9A9A9;
+    }
+
+    th, td {
+        padding: 15px;
+        text-align: left;
+    }
+
+    th {
+        height: 50px;
+        background-color: #20B2AA;
+        color: white;
+    }
+
+</style>
